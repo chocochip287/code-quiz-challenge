8e81671287e31d5ef9f6ed7c026c657c7ca61b9c
Matt <39172048+magiccreb@users.noreply.github.com>
2022-10-11 20:24:31 -0400

    set up answers object and a few core variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
+// base level variables
+
+var body = document.body;
+var header = document.createElement("header");
+var h1El = document.createElement("h1");
+
 // this is the starting point for this commentary,
-// please add more steps
 // there may be more steps and corrections to be made in future
-console.log("Sam, your questions");
 // need a collection of objects that hold the questions
 var questionsObj = {
   questions: ["Which of the following is the primary role of JavaScript in web design?", "What is the result of the operation 4 % 2?", "What does the pop() method do to an array?", "What will the console output if a user enters 'console.log(isNaN(5));?", "Which option increases the value of i by one each time it occurs?"],
 }
 
 // create objects containing the answer sets for each question
-
+var answersObj = {
+  answers0: ["Defining the content of a webpage", "Setting the layout of a webpage", "Adding interactivity and behavior to a webpage", "Ensuring a webpage appears in search engines"],
+  answers1: ["!"],
+  answers2: ["?"],
+  answers3: ["@"],
+  answers4: ["%"]
+}
 
 // need to keep a count of the question that is displayed
 // need to keep score
