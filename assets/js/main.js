1b2aa96ca7674ddaffa7f0ea47c01b27ae0fcf7d
Matt <goldsmith.matt@gmail.com>
2022-10-16 19:50:45 -0400

    added functionality to start button with placeholder text. experimenting with timer.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,8 @@ var anchorEl = document.createElement("a");
 var mainDiv = document.createElement("div");
 var pEl = document.createElement("p");
 var h1El = document.createElement("h1");
-var startButton = document.createElement("button");
+var startButton = document.createElement("input");
+var timerButton = document.createElement("input");
 
 // core text content
 
@@ -16,7 +17,6 @@ anchorEl.textContent = "this will be the high scores link";
 navDivEl.textContent = "the timer will go here";
 h1El.textContent = "Coding Quiz Challenge";
 pEl.textContent = "Try to answer the following JavaScript related questions before time runs out. Correct answers will extend your time. Incorrect answers will reduce it.";
-startButton.textContent = "Start!";
 
 // class and id selector variables, if I end up using them.
 
@@ -26,6 +26,7 @@ body.appendChild(mainDiv);
 mainDiv.appendChild(h1El);
 mainDiv.appendChild(pEl);
 mainDiv.appendChild(startButton);
+mainDiv.appendChild(timerButton);
 header.appendChild(navEl);
 navEl.appendChild(anchorEl);
 navEl.appendChild(navDivEl);
@@ -35,7 +36,10 @@ body.setAttribute("style", "background: rgb(106, 121, 149); height: 95vh;");
 header.setAttribute("style", "margin: 5px; border: 2px solid white")
 mainDiv.setAttribute("style", "padding: 30px; border: 2px solid black; margin: 5px; display: flex; justify-content: center; align-items: center; flex-direction: column;");
 mainDiv.setAttribute("class", "mainDiv");
-navEl.setAttribute("style", "display: flex; justify-content: space-between;")
+startButton.setAttribute("type", "button");
+startButton.setAttribute("value", "Start!");
+timerButton.setAttribute("type", "button");
+navEl.setAttribute("style", "display: flex; justify-content: space-between;");
 navDivEl.setAttribute("style", "border: solid 1px red")
 // be sure to change the google placeholder link to a high scores link
 anchorEl.setAttribute("href", "https://www.google.com");
@@ -90,12 +94,14 @@ function addQuestion(){
     // go to highscore screen
 }
 
-function startQuiz(){
+// event listener that begins the quiz on clicking start
+
+startButton.addEventListener("click", function startQuiz() {
   // timer starts and I am presented with a question
   // need a function call to start the timer
-
+  // startTimer();
+  h1El.textContent = "ayo nice job clicking that start button"
+  pEl.textContent = "but now you're gonna need to refresh, my guy."
   // need a function call to present the question
-}
-
-// eventListener here // when i click the start button
+});
 
