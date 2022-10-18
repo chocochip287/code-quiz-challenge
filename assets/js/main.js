4556be8b3a3b69f14d64f31d8be2a9f823073829
Matt <39172048+magiccreb@users.noreply.github.com>
2022-10-18 11:13:56 -0400

    removed timer test button. thorough commentary cleanup and updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,37 @@
+/*
+10/18/22
+- Score doesn't display during the quiz - only at the end.
+- Need to set up question displays and inputs. Set up right/wrong feedback responses with a delay before presenting next question. Use a function to respond right/wrong depending on the answer.
+- Style the entire app.
+
+
+*/
+
 // core variables
 
 var body = document.body;
 var header = document.createElement("header");
 var navEl = document.createElement("nav");
-var navDivEl = document.createElement("div");
+var navDivTime = document.createElement("div");
 var anchorEl = document.createElement("a");
 var mainDiv = document.createElement("div");
 var pEl = document.createElement("p");
 var h1El = document.createElement("h1");
+var timeLeft = "xx";
+var score = 0;
+
+// buttons
 var startButton = document.createElement("input");
 var timerButton = document.createElement("input");
-var timeLeft = "xx";
+var a1Button = document.createElement("input");
+var a2Button = document.createElement("input");
+var a3Button = document.createElement("input");
+var a4Button = document.createElement("input");
 
 // core text content
 
 anchorEl.textContent = "this will be the high scores link";
-navDivEl.textContent = "Time left: " + timeLeft;
+navDivTime.textContent = "Time left: " + timeLeft;
 h1El.textContent = "Coding Quiz Challenge";
 pEl.textContent = "Try to answer the following JavaScript related questions before time runs out. Correct answers will extend your time. Incorrect answers will reduce it.";
 
@@ -27,31 +43,31 @@ body.appendChild(mainDiv);
 mainDiv.appendChild(h1El);
 mainDiv.appendChild(pEl);
 mainDiv.appendChild(startButton);
-mainDiv.appendChild(timerButton);
 header.appendChild(navEl);
 navEl.appendChild(anchorEl);
-navEl.appendChild(navDivEl);
+navEl.appendChild(navDivTime);
 
 // setting up attributes for the appended elements
+
 body.setAttribute("style", "background: rgb(106, 121, 149); height: 95vh;");
 header.setAttribute("style", "margin: 5px; border: 2px solid white")
 mainDiv.setAttribute("style", "padding: 30px; border: 2px solid black; margin: 5px; display: flex; justify-content: center; align-items: center; flex-direction: column;");
 mainDiv.setAttribute("class", "mainDiv");
 startButton.setAttribute("type", "button");
 startButton.setAttribute("value", "Start!");
-timerButton.setAttribute("type", "button");
-timerButton.setAttribute("value", "run the timer");
 navEl.setAttribute("style", "display: flex; justify-content: space-between;");
-navDivEl.setAttribute("style", "border: solid 1px red")
+navDivTime.setAttribute("style", "border: solid 1px red")
 // be sure to change the google placeholder link to a high scores link
 anchorEl.setAttribute("href", "https://www.google.com");
 
-// need a collection of objects that hold the questions
+// object that contains the quiz questions
+
 var questionsObj = {
   questions: ["Which of the following is the primary role of JavaScript in web design?", "What is the result of the operation 4 % 2?", "What does the pop() method do to an array?", "What will the console output if a user enters 'console.log(isNaN(5));?", "Which option increases the value of i by one each time it occurs?"],
 }
 
-// create objects containing the answer sets for each question
+// object that contains answers that correspond to the index of the related question from questionsObj
+
 var answersObj = {
   answers0: ["Defining the content of a webpage", "Setting the layout of a webpage", "Adding interactivity and behavior to a webpage", "Ensuring a webpage appears in search engines"],
   answers1: ["!"],
@@ -60,23 +76,19 @@ var answersObj = {
   answers4: ["%"]
 }
 
-// need to keep a count of the question that is displayed
-// need to keep score
-// need to keep the time
-
-// function to start the timer
-timerButton.addEventListener("click", function startTimer(){
+// function to start the timer and begin the quiz
+startButton.addEventListener("click", function startTimer(){
   timeLeft = 10;
-  navDivEl.textContent = "Time left: " + timeLeft + " seconds.";
+  navDivTime.textContent = "Time left: " + timeLeft + " seconds.";
   var testTimer = setInterval(function () {
     if (timeLeft > 1) {
-      navDivEl.textContent = "Time left: " + timeLeft + " seconds.";
+      navDivTime.textContent = "Time left: " + timeLeft + " seconds.";
       timeLeft--;
     } else if (timeLeft === 1) {
-      navDivEl.textContent = "Time left: " + timeLeft + " second.";
+      navDivTime.textContent = "Time left: " + timeLeft + " second.";
       timeLeft--;
     } else {
-      navDivEl.textContent = "Time's up!";
+      navDivTime.textContent = "Time's up!";
       timesUp();
       clearInterval();
     }
@@ -84,6 +96,7 @@ timerButton.addEventListener("click", function startTimer(){
 });
 
 // function to present and cycle questions
+
 function addQuestion(){
   // create the question elements
 
@@ -94,36 +107,14 @@ function addQuestion(){
 
   // for all the buttons, add event listener
 
-    // WHEN I answer a question
-    // if answered correctly
-    // add points
-    // show as correct
-    // WHEN I answer a question incorrectly
-    // THEN time is subtracted from the clock
-    // show as incorrect
-
-    // after the answer, 
-    // THEN I am presented with another question
-    // need a function call to present the question
-
-    // if no other questions,
-    // go to highscore screen
 }
 
 // event listener that begins the quiz on clicking start
-
-startButton.addEventListener("click", function startQuiz() {
-  // timer starts and I am presented with a question
-  // need a function call to start the timer
-  // startTimer();
-  h1El.textContent = "ayo nice job clicking that start button"
-  pEl.textContent = "but now you're gonna need to refresh, my guy."
-  // need a function call to present the question
-});
 
 // function that runs if the user runs out of time.
 
 function timesUp() {
   h1El.textContent = "oh noes, you ran out of time!";
+  // set up div content to allow the usual score display and name entry
 
 }
