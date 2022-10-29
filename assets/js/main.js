646fe6f17c9d7e4a4e4961880278fc68dd26cab0
Matt <goldsmith.matt@gmail.com>
2022-10-29 17:49:14 -0400

    inputs adjusted to buttons. return home functionality established and added to timeout logic. some adjustments to q&a objects pre-implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,19 +21,22 @@ var resultDiv = document.createElement("div");
 var buttonDiv = document.createElement("div");
 // empty array to be used for locally storing scores
 var scores = [];
+// global variable to count question number
+var questionCounter
 
 // input and submission button for scores
 var initInput = document.createElement("input");
 var scoreSubmit = document.createElement("input");
 
 // more buttons
-var startButton = document.createElement("input");
-var a1Button = document.createElement("input");
-var a2Button = document.createElement("input");
-var a3Button = document.createElement("input");
-var a4Button = document.createElement("input");
+var startButton = document.createElement("button");
+var a1Button = document.createElement("button");
+var a2Button = document.createElement("button");
+var a3Button = document.createElement("button");
+var a4Button = document.createElement("button");
+var homeButton = document.createElement("button");
 // special button for testing page content and function - not intended for final release
-var testButton = document.createElement("input");
+var testButton = document.createElement("button");
 
 // test misc variables 
 var timeLeft = "xx";
@@ -46,6 +49,13 @@ anchorEl.textContent = "this will be the high scores link";
 navDivTime.textContent = "Time left: " + timeLeft;
 h1El.textContent = "Coding Quiz Challenge";
 pEl.textContent = "Try to answer the following JavaScript related questions before time runs out. Correct answers will extend your time while incorrect answers will reduce it.";
+startButton.textContent = "Start!"
+testButton.textContent = "test"
+a1Button.textContent = "1";
+a2Button.textContent = "2";
+a3Button.textContent = "3";
+a4Button.textContent = "4";
+homeButton.textContent = "Go home?"
 
 // class and id selector variables, if I end up using them.
 
@@ -72,51 +82,40 @@ mainDiv.setAttribute("style", "padding: 30px; border: 2px solid black; margin: 5
 mainDiv.setAttribute("class", "mainDiv");
 buttonDiv.setAttribute("style", "display: flex; justify-content: ")
 startButton.setAttribute("class", "button")
-startButton.setAttribute("type", "button");
-startButton.setAttribute("value", "Start!");
-testButton.setAttribute("class", "button");
-testButton.setAttribute("type", "button");
-testButton.setAttribute("value", "test");
+testButton.setAttribute("class", "hide-me");
 a1Button.setAttribute("class", "button");
-a1Button.setAttribute("type", "button");
-a1Button.setAttribute("value", "1");
 a2Button.setAttribute("class", "button");
-a2Button.setAttribute("type", "button");
-a2Button.setAttribute("value", "2");
 a3Button.setAttribute("class", "button");
-a3Button.setAttribute("type", "button");
-a3Button.setAttribute("value", "3");
 a4Button.setAttribute("class", "button");
-a4Button.setAttribute("type", "button");
-a4Button.setAttribute("value", "answer 4");
+homeButton.setAttribute("class", "hide-me");
 navEl.setAttribute("style", "display: flex; justify-content: space-between;");
 navDivTime.setAttribute("style", "border: solid 1px red")
 // be sure to change the google placeholder link to a high scores link
 anchorEl.setAttribute("href", "https://www.google.com");
 
 var questionsObj = {
-  questions: ["Which of the following is the primary role of JavaScript in web design?", "What is the result of the operation 4 % 2?", "What does the pop() method do to an array?", "What will the console output if a user enters 'console.log(isNaN(5));?", "Which option increases the value of i by one each time it occurs?"],
+  questions: ["Which of the following is the primary role of JavaScript in web design?", "What is the result of the operation 4 % 2?", "What does the pop() method do to an array?", "What will the console output if a user enters 'console.log(isNaN(5));?", "Which option increases the value of i by one each time it occurs?"]
 }
 
 // object containing answers that correspond to the index of the related question from questionsObj
 
 var answersObj = {
   // [2] is correct
-  answers0: ["Defining the content of a webpage", "Setting the layout of a webpage", "Adding interactivity and behavior to a webpage", "Ensuring a webpage appears in search engines"],
+  q0: ["Defining the content of a webpage", "Setting the layout of a webpage", "Adding interactivity and behavior to a webpage", "Ensuring a webpage appears in search engines"],
   // [0] is correct
-  answers1: ["0", "2", "NaN", ".5"],
+  q1: ["0", "2", "NaN", ".5"],
   // [1] is correct
-  answers2: ["Duplicates all values in the array", "Removes the last element of an array", "Deletes the entire array", "Changes the array values to Starburst flavors"],
+  q2: ["Duplicates all values in the array", "Removes the last element of an array", "Deletes the entire array", "Changes the array values to Starburst flavors"],
   // [3] is correct
-  answers3: ["true", "5", "Go Eagles!", "false"],
+  q3: ["true", "5", "Go Eagles!", "false"],
   // [1] is correct
-  answers4: ["i + 5", "i--", "i++", "i**"]
+  q4: ["i + 5", "i--", "i++", "i**"]
 }
 
 // function to start the timer and begin the quiz
-startButton.addEventListener("click", function startTimer(){
+startButton.addEventListener("click", function beginTest(){
   timeLeft = 10;
-  testQuestions();
+  testPhase();
   navDivTime.textContent = "Time left: " + timeLeft + " seconds.";
   var testTimer = setInterval(function () {
     if (timeLeft > 1) {
@@ -128,24 +127,22 @@ startButton.addEventListener("click", function startTimer(){
     } else {
       navDivTime.textContent = "Time's up!";
       timesUp();
-      clearInterval();
+      clearInterval(testTimer);
     }
   }, 1000);
 });
 
-// function to present and cycle questions
+// function to transition from the home screen to the test
 
-function testQuestions(){
-  for (let i = 0; (i < questionsObj.questions.length) ; i++) {
-    h1El.textContent = questionsObj.questions[i];
+function testPhase(){
     startButton.setAttribute("class", "hide-me");
     pEl.setAttribute("class", "hide-me");
     mainDiv.appendChild(a1Button);
     mainDiv.appendChild(a2Button);
     mainDiv.appendChild(a3Button);
     mainDiv.appendChild(a4Button);
-  }
 }
+
 
 // function that runs if the user runs out of time.
 
@@ -153,14 +150,26 @@ function timesUp() {
   h1El.textContent = "oh noes, you ran out of time!";
   // set up div content to allow the usual score display and name entry
 
-}
+  // set up "go home" functionality
+  mainDiv.removeChild(a1Button);
+  mainDiv.removeChild(a2Button);
+  mainDiv.removeChild(a3Button);
+  mainDiv.removeChild(a4Button);
+  mainDiv.appendChild(homeButton);
+  homeButton.setAttribute("class", "button");
+};
+
 
 // function to return to the start screen - this needs to return everything on the screen to the beginning without removing scores
 
-testButton.addEventListener("click", function goHome() {
+homeButton.addEventListener("click", function goHome() {
   h1El.textContent = "Coding Quiz Challenge";
   startButton.setAttribute("class", "button");
   timeLeft = "xx";
+  navDivTime.textContent = "Time left: " + timeLeft;
+  startButton.setAttribute("class", "button");
+  pEl.setAttribute("class", "");
+  homeButton.setAttribute("class", "hide-me");
 });
 
 /*
